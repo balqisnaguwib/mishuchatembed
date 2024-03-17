c94904ad43af34b3bc9e3246cc333f5837b324c6
balqisnaguwib <balqisnaguwib@gmail.com>
2024-03-17 14:18:07 +0800

    Edit X-Icon

--- src/components/icons/XIcon.tsx
+++ src/components/icons/XIcon.tsx
@@ -1,5 +1,6 @@
 import { JSX } from 'solid-js/jsx-runtime';
 const defaultButtonColor = '#3B81F6';
+
 export const XIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +13,6 @@ export const XIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     stroke-linecap="round"
     stroke-linejoin="round"
   >
-    <path d="M18 6 6 18" />
-    <path d="m6 6 12 12" />
+    <path d="M10 4v4l-6-2L1 7l5 5-5 5 3 1 6-2v4h2v-4l6 2 2-1-5-5 5-5-3-1-6 2V4h-2z" />
   </svg>
 );
